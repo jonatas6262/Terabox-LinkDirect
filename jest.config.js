f753bd9437f9a5e3d0a0adca9a80616ca71390bf
jonatas6262 <52454280+jonatas6262@users.noreply.github.com>
2023-12-10 12:05:28 -0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: "miniflare",
-    transform: {
-        "^.+\\.(ts|tsx)$": "esbuild-jest",
-    },
-    moduleNameMapper: {
-        __STATIC_CONTENT_MANIFEST: "<rootDir>/tests/manifest.ts",
-    },
-}
